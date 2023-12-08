06ddf7ecf8e52af3af2b07e4e433d3bc02596914
thisiskeanyvy <notifkeanyvy@gmail.com>
2023-12-08 10:24:08 +0100

    readme logos edit

--- readme.py
+++ readme.py
@@ -11,14 +11,14 @@ with open('README.md', 'a') as readme_file:
     # Ajout de la section des logos sous forme de tableau
     readme_file.write("\n\n---\n\n")
     readme_file.write("\n## Services déjà intégrés\n\n")
-    readme_file.write("| Service 1 | Service 2 | Service 3 | Service 4 | Service 5 |" + "\n")
-    readme_file.write("| --- " * 5 + "|" "\n")
+    readme_file.write("| Service 1 | Service 2 | Service 3 | Service 4 | Service 5 | Service 6 | Service 7 | Service 8 |" + "\n")
+    readme_file.write("| --- " * 8 + "|" "\n")
     
-    for i in range(0, len(logo_files), 5):
+    for i in range(0, len(logo_files), 8):
         # Construction des balises Markdown pour les logos dans la ligne
         logos_in_row = [
             f'<img src="{os.path.join(logo_directory, logo_file)}" alt="{os.path.splitext(logo_file)[0]}" style="width: 56px; height: 56px; border-radius: 12px;" />'
-            for logo_file in logo_files[i:i+5]
+            for logo_file in logo_files[i:i+8]
         ]
         
         # Ajout de la ligne dans le tableau
@@ -29,14 +29,14 @@ with open('README-en.md', 'a') as readme_file:
     # Ajout de la section des logos sous forme de tableau
     readme_file.write("\n\n---\n\n")
     readme_file.write("\n## Services already integrated\n\n")
-    readme_file.write("| Service 1 | Service 2 | Service 3 | Service 4 | Service 5 |" + "\n")
-    readme_file.write("| --- " * 5 + "|" "\n")
+    readme_file.write("| Service 1 | Service 2 | Service 3 | Service 4 | Service 5 | Service 6 | Service 7 | Service 8 |" + "\n")
+    readme_file.write("| --- " * 8 + "|" "\n")
     
-    for i in range(0, len(logo_files), 5):
+    for i in range(0, len(logo_files), 8):
         # Construction des balises Markdown pour les logos dans la ligne
         logos_in_row = [
             f'<img src="{os.path.join(logo_directory, logo_file)}" alt="{os.path.splitext(logo_file)[0]}" style="width: 56px; height: 56px; border-radius: 12px;" />'
-            for logo_file in logo_files[i:i+5]
+            for logo_file in logo_files[i:i+8]
         ]
         
         # Ajout de la ligne dans le tableau
